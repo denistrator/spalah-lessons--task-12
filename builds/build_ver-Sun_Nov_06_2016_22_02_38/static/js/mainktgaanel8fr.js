0644a594e02c4f709ff5b2aa9844d590bec5a447
denis <denistrator@yandex.ua>
2016-11-06 22:07:54 +0200

    update compiled project

--- builds/build_ver-Sun_Nov_06_2016_22_02_38/static/js/mainktgaanel8fr.js
+++ builds/build_ver-Sun_Nov_06_2016_22_02_38/static/js/mainktgaanel8fr.js
@@ -4636,6 +4636,39 @@ else if (typeof define === 'function' && define.amd) {
 
 
 
+$(document).ready(function() {
+    var clients_carousel = new Swiper('.clients-carousel', {
+        loop: true,
+        prevButton: '.clients-carousel-button-prev',
+        nextButton: '.clients-carousel-button-next',
+        // Default parameters
+         slidesPerView: 6,
+         spaceBetween: 40,
+         // Responsive breakpoints
+         breakpoints: {
+           // when window width is <= 320px
+           320: {
+             slidesPerView: 2,
+             spaceBetween: 10
+           },
+           // when window width is <= 480px
+           480: {
+             slidesPerView: 2,
+             spaceBetween: 15
+           },
+           // when window width is <= 640px
+           640: {
+             slidesPerView: 4,
+             spaceBetween: 20
+           },
+           // when window width is <= 960px
+           960: {
+             slidesPerView: 6,
+             spaceBetween: 30
+           }
+         }
+    })
+});
 
 $(document).ready(function() {
     var m_gallery = new Swiper('.m-gallery', {
